ed7bcb44e2fdaee6af3cb885e55567f48adae907
wrapper228 <32472244+wrapper228@users.noreply.github.com>
2026-01-26 04:26:38 +0300

    remove cosine part

--- final_tex_paper_in_this_folder_mlsys26/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-cd "/workspace/final_tex_paper_in_this_folder"
-pdflatex -interaction=nonstopmode paper.tex || true
-bibtex paper || true
-pdflatex -interaction=nonstopmode paper.tex || true
-pdflatex -interaction=nonstopmode paper.tex || true
